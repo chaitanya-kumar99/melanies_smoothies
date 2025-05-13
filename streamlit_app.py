0e501ab4e6be3ff404f98dd3aa6ee74d37a9edc9
chaitanya-kumar99 <152267886+chaitanya-kumar99@users.noreply.github.com>
2025-05-13 16:20:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,10 @@ session = Session.builder.configs(connection_parameters).create()
 
 # Fetch available fruits from Snowflake
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 # Create a multiselect to choose ingredients (up to 5 ingredients)
